ae830f36f07252959fe0518d73b9cc0f1aa08e3f
Andy Hudson <andyhudson1979@gmail.com>
2024-08-02 21:01:36 +0100

    added a remote app

--- apps/host-app/src/app/app.tsx
+++ apps/host-app/src/app/app.tsx
@@ -4,6 +4,8 @@ import NxWelcome from './nx-welcome';
 
 import { Link, Route, Routes } from 'react-router-dom';
 
+const RemoteApp = React.lazy(() => import('remote-app/Module'));
+
 export function App() {
   return (
     <React.Suspense fallback={null}>
@@ -11,9 +13,13 @@ export function App() {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/remote-app">RemoteApp</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<NxWelcome title="host-app" />} />
+        <Route path="/remote-app" element={<RemoteApp />} />
       </Routes>
     </React.Suspense>
   );
